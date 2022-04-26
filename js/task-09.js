f21e7ac5768ce46cd6b7514cbc9ebebd917e9532
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-04-26 21:21:00 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const btnChangeColor = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+
+btnChangeColor.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+}
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
