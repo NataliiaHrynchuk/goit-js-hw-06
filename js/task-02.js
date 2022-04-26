a02038a85e1928a04995c1ad46da44f7d70b2d86
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-04-26 10:54:47 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.getElementById('ingredients');
+console.log(ingredients);
+ingredients.map((ingredient) => {
+  const newEl = document.createElement('li');
+  newEl.className = 'item';
+  newEl.textContent = ingredient;
+  list.append(newEl);
+
+})
+
+console.log(list);
